5608dc30b788726fe87027597bf17a966de8912b
kastenbauer27 <pkast2016@comcast.net>
2020-11-22 15:52:00 -0600

    made function to append tasks to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,23 +2,37 @@ console.log('JS is sourced');
 
 $(document).ready(onReady);
 
+// function to call jquery, establish click handlers, and get taskList on page load
 function onReady() {
     console.log('jquery sourced and loaded!');
     getTasks();
 }
 
+// function to send GET request to server, gets response of all tasks
 function getTasks() {
     $.ajax({
         method: 'GET',
         url: '/tasks'
     }).then( response => {
-        let taskList = response;
-        console.log(taskList);
+        let tasks = response;
+        console.log(tasks);
+        renderTaskList(tasks);
     }).catch( err => {
         console.log('Error in getting tasks', err);
     })
 }
 
+// function to append all tasks from database to DOM
+function renderTaskList(taskList) {
+    for (let task of taskList) {
+        $('#taskList').append(`
+        <tr>
+        <td>${task.taskInfo}</td>
+        <td>${task.complete}</td>
+        </tr>
+        `);
+    }
+}
 
 
 
